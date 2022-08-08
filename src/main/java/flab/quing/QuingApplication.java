2aa0ba23b67e8645a386d3e42c89d033142816ec
Chanwoo Kim <chanwookim@me.com>
2022-08-08 22:34:36 +0900

    init commit

---
+++ src/main/java/flab/quing/QuingApplication.java
@@ -0,0 +1,13 @@
+package flab.quing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/flab/quing/QuingApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.quing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
