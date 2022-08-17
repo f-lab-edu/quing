083d29363f4a62c9b08e5a3765afdc0361789de1
yuseon <llys0331@gmail.com>
2022-08-17 17:05:14 +0900

    feature/Member 도메인 작성

--- src/main/java/flab/quing/domain/Member.java
+++ src/main/java/flab/quing/domain/Member.java
@@ -1,4 +1,15 @@
 package flab.quing.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
 public class Member {
+    private Long id;
+    private String name;
+    private String password;
+    private String phone;
 }
