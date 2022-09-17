c2c79670296df3f81de3c2e880e42fa3b2d7ee74
Chanwookim <chanwookim@me.com>
2022-09-18 00:19:17 +0900

    review반영/self-descriptive한 method명 updateRating->updateAll()

--- src/main/java/flab/quing/review/RatingService.java
+++ src/main/java/flab/quing/review/RatingService.java
@@ -6,9 +6,6 @@ public interface RatingService {
 
     RatingResponse getRating(Long storeId);
 
-    /**
-     * 모든 상점의 리뷰와 별점을 계산해서 각 상점별 별점을 갱신합니다.
-     */
-    void updateRating();
+    void updateAll();
 
 }
