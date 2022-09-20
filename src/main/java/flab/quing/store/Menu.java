edb576a0d9c7a88e2bb46a4cbcd6039693677e90
yuseon <llys0331@gmail.com>
2022-09-20 15:41:15 +0900

    review반영/Entity Lombok Annotation 적용

--- src/main/java/flab/quing/store/Menu.java
+++ src/main/java/flab/quing/store/Menu.java
@@ -1,20 +1,27 @@
 package flab.quing.store;
 
 import flab.quing.user.BaseEntity;
-import lombok.Getter;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 
+@Data
+@Setter(AccessLevel.NONE)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Getter
-@Setter
 public class Menu extends BaseEntity {
     @ManyToOne(cascade = CascadeType.ALL)
     private Store store;
     private String name;
     private Integer price;
-    private String image;
+    private String imageUrl;
 }
--- src/main/java/flab/quing/user/StoreManager.java
+++ src/main/java/flab/quing/user/StoreManager.java
@@ -1,4 +1,28 @@
 package flab.quing.user;
 
-public class StoreManager {
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+
+@Data
+@Setter(AccessLevel.NONE)
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class StoreManager extends BaseEntity {
+
+    private String loginId;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String name;
 }
