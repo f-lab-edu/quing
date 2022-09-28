32afc1250e27f1f038dccc671cf8d07c94b2e012
Chanwookim <chanwookim@me.com>
2022-09-29 05:07:28 +0900

    feature/soft delete를 위한 필드 추가

--- src/main/java/flab/quing/user/BaseEntity.java
+++ src/main/java/flab/quing/user/BaseEntity.java
@@ -1,16 +1,22 @@
 package flab.quing.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 @MappedSuperclass
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+    private boolean deleted;
     private LocalDateTime created_at = LocalDateTime.now();
     private LocalDateTime updated_at;
     private LocalDateTime deleted_at;
