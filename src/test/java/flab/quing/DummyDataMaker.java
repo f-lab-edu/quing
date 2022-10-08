20b903081e6d3aff1f707298649df37cc3cd06db
Chanwookim <chanwookim@me.com>
2022-10-08 17:17:27 +0900

    #15-Review repository, service test 작성

--- src/test/java/flab/quing/DummyDataMaker.java
+++ src/test/java/flab/quing/DummyDataMaker.java
@@ -1,23 +1,93 @@
 package flab.quing;
 
+import flab.quing.review.Review;
 import flab.quing.store.Store;
 import flab.quing.user.User;
+import flab.quing.waiting.Waiting;
+import flab.quing.waiting.WaitingQueueStatus;
+
+import java.util.List;
 
 public class DummyDataMaker {
 
-    static User member() {
-        User member = new User();
-        return member;
+    static long userCount, storeCount, waitingCount, reviewCount;
+
+    public static void init() {
+        userCount = 0;
+        storeCount = 0;
+        waitingCount = 0;
+        reviewCount = 0;
     }
 
-    static User member(String name) {
-        User member = DummyDataMaker.member();
-        return member;
+    public static User user() {
+        userCount++;
+        User user = User.builder()
+                .name("TestUser" + userCount)
+                .phone("010-1234-" + String.format("%04d", userCount))
+                .build();
+        user.setId(userCount);
+        return user;
     }
 
-    static Store store() {
-        Store store = new Store();
+    public static User user(String name) {
+        userCount++;
+        User user = User.builder()
+                .name(name)
+                .phone("010-1234-" + String.format("%04d", userCount))
+                .build();
+        user.setId(userCount);
+        return user;
+    }
+
+    public static Store store() {
+        storeCount++;
+        Store store = Store.builder()
+                .name("TestStore" + storeCount)
+                .phoneNumber("010-1234-" + String.format("%04d", storeCount))
+                .address("서울특별시 강남구 강남대로 " + String.format("%04d", storeCount))
+                .openHours("매일 09:00~18:00")
+                .openStatus("영업중/마감")
+                .pageLink("storeLink")
+                .build();
+        store.setId(storeCount);
         return store;
     }
 
+    public static Waiting waiting(User user, Store store) {
+        waitingCount++;
+        Waiting waiting = Waiting.builder()
+                .user(user)
+                .store(store)
+                .waitingQueueStatus(WaitingQueueStatus.WAITING)
+                .callCount(0)
+                .build();
+        waiting.setId(waitingCount);
+        return waiting;
+    }
+
+    public static Review review(User user, Waiting waiting) {
+        reviewCount++;
+        Review review = Review.builder()
+                .user(user)
+                .waiting(waiting)
+                .message("review" + reviewCount)
+                .rating(10)
+                .imageUrls(List.of("link1", "link2"))
+                .build();
+        review.setId(reviewCount);
+        return review;
+    }
+
+    public static Review review(User user, Waiting waiting, String message) {
+        reviewCount++;
+        Review review = Review.builder()
+                .user(user)
+                .waiting(waiting)
+                .message(message)
+                .rating(10)
+                .imageUrls(List.of("link1", "link2"))
+                .build();
+        review.setId(reviewCount);
+        return review;
+    }
 }
--- src/test/java/flab/quing/review/ReviewServiceImplTest.java
+++ src/test/java/flab/quing/review/ReviewServiceImplTest.java
@@ -1,5 +1,6 @@
 package flab.quing.review;
 
+import flab.quing.DummyDataMaker;
 import flab.quing.review.dto.ReviewRequest;
 import flab.quing.review.dto.ReviewResponse;
 import flab.quing.store.Store;
@@ -8,12 +9,15 @@ import flab.quing.user.User;
 import flab.quing.waiting.Waiting;
 import flab.quing.waiting.WaitingQueueStatus;
 import flab.quing.waiting.WaitingRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,6 +38,11 @@ class ReviewServiceImplTest {
     @Mock
     WaitingRepository waitingRepository;
 
+
+    @BeforeEach
+    void init() {
+        DummyDataMaker.init();
+    }
 
     @Test
     void create_성공() {
@@ -66,7 +75,7 @@ class ReviewServiceImplTest {
                 .waitingId(waiting.getStore().getId())
                 .rating(3)
                 .message("그냥그래요")
-                .imageUrl("https://user-images.githubusercontent.com/22807730/191471325-862134f8-6d8f-4062-9b12-ecd541a6323a.png")
+                .imageUrls(List.of("https://user-images.githubusercontent.com/22807730/191471325-862134f8-6d8f-4062-9b12-ecd541a6323a.png"))
                 .build();
 
         Review review = Review.of(user, waiting, reviewRequest);
@@ -89,27 +98,118 @@ class ReviewServiceImplTest {
     void update() {
         //이전 기록이 deleted가 잘 되는지(안나오는지)
         //수정한 내역이 새로 잘 만들어지는지
+
+        User user = DummyDataMaker.user();
+        Store store = DummyDataMaker.store();
+        Waiting waiting = DummyDataMaker.waiting(user, store);
+
+        Review review1 = DummyDataMaker.review(user, waiting, "review1");
+        Review review2 = DummyDataMaker.review(user, waiting, "review2");
+
+        ReviewRequest reviewRequest1 = ReviewRequest.builder()
+                .userId(user.getId())
+                .waitingId(waiting.getId())
+                .message("review1")
+                .build();
+
+        ReviewRequest reviewRequest2 = ReviewRequest.builder()
+                .userId(user.getId())
+                .waitingId(waiting.getId())
+                .message("review2")
+                .build();
+
+        //create
+        when(waitingRepository.findById(anyLong())).thenReturn(Optional.of(waiting));
+        when(reviewRepository.findById(anyLong())).thenReturn(Optional.of(review1));
+        when(reviewRepository.save(any(Review.class)))
+                .thenReturn(review1)    // 1st create
+                .thenReturn(review2);   // 2nd create
+
+        //update
+        when(reviewRepository.findTopByWaitingOrderByIdDesc(waiting)).thenReturn(Optional.of(review1));
+
+
+        ReviewResponse createResponse = reviewService.create(reviewRequest1);
+        ReviewResponse updateResponse = reviewService.update(reviewRequest2);
+
+        assertThat(updateResponse.getMessage()).isEqualTo("review2");
     }
 
     @Test
     void hide() {
         //delete필드가 잘 바뀌는지?
+        User user = DummyDataMaker.user();
+        Store store = DummyDataMaker.store();
+        Waiting waiting = DummyDataMaker.waiting(user, store);
+        Review review = DummyDataMaker.review(user, waiting);
+
+        when(reviewRepository.findById(anyLong())).thenReturn(Optional.of(review));
+
+        ReviewResponse response = reviewService.hide(review.getId());
+
+        System.out.println("response = " + response);
+        System.out.println("review = " + review.isDeleted());
+        Assertions.assertThat(review.isDeleted()).isEqualTo(true);
+
+
     }
 
     @Test
     void get() {
         //review id로 review를 잘 가져오는지..
+        User user = DummyDataMaker.user();
+        Store store = DummyDataMaker.store();
+        Waiting waiting = DummyDataMaker.waiting(user, store);
+        Review review = DummyDataMaker.review(user, waiting);
+
+        when(reviewRepository.findById(anyLong())).thenReturn(Optional.of(review));
+
+        ReviewResponse reviewResponse = reviewService.get(review.getId());
+        System.out.println("reviewResponse = " + reviewResponse);
+        assertThat(reviewResponse.getMessage()).isEqualTo("review1");
+
     }
 
     @Test
     void find() {
         //waiting_id로 (deleted 필드가 false인) 가장 최근 review를 가져오는지
         //deleted필드를 검사하진 않지만 false여야한다.
+        User user = DummyDataMaker.user();
+        Store store = DummyDataMaker.store();
+        Waiting waiting = DummyDataMaker.waiting(user, store);
+        Review review = DummyDataMaker.review(user, waiting);
+
+        when(waitingRepository.findById(anyLong())).thenReturn(Optional.of(waiting));
+        when(reviewRepository.findTopByWaitingOrderByIdDesc(waiting)).thenReturn(Optional.of(review));
+
+        ReviewResponse response = reviewService.find(waiting.getId());
+        System.out.println("response = " + response);
+
+        assertThat(response).isEqualTo(review.toResponse());
+
 
     }
 
     @Test
     void getList() {
         //상점 id로 deleted 필드가 false인 리뷰 리스트를 가져온다.
+        User user1 = DummyDataMaker.user();
+        User user2 = DummyDataMaker.user();
+        Store store = DummyDataMaker.store();
+        Waiting waiting1 = DummyDataMaker.waiting(user1, store);
+        Waiting waiting2 = DummyDataMaker.waiting(user2, store);
+        Review review1 = DummyDataMaker.review(user1, waiting1);
+        Review review2 = DummyDataMaker.review(user2, waiting2);
+
+        when(storeRepository.findById(anyLong())).thenReturn(Optional.of(store));
+        when(reviewRepository.findAllByWaitingStoreAndDeletedIsFalse(store)).thenReturn(List.of(review1, review2));
+
+        List<ReviewResponse> responseList = reviewService.getList(store.getId());
+        responseList.stream().forEach(System.out::println);
+
+        assertThat(responseList.size()).isEqualTo(2);
+        assertThat(responseList.get(0).getStoreName()).isEqualTo("TestStore1");
+
+
     }
 }
