5dce4ea4e2f8bde4c8db2c5128fe3add3ac8e35c
Chanwookim <chanwookim@me.com>
2022-10-08 17:15:40 +0900

    #15-Review waiting으로 find 해올때 1개의 결과만 가져오도록 limit 설정

--- src/main/java/flab/quing/review/ReviewRepository.java
+++ src/main/java/flab/quing/review/ReviewRepository.java
@@ -10,6 +10,6 @@ import java.util.Optional;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByWaitingStoreAndDeletedIsFalse(Store store);
 
-    Optional<Review> findByWaitingOrderByIdDesc(Waiting waiting);
+    Optional<Review> findTopByWaitingOrderByIdDesc(Waiting waiting);
 
 }
