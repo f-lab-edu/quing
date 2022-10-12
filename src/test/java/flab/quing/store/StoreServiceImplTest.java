eb3a47a669e082a62da0c79537a57069f6b695df
yuseon <llys0331@gmail.com>
2022-10-13 00:42:54 +0900

    #17-메뉴저장,수정 및 스토어 저장 테스트코드 작성

--- src/test/java/flab/quing/store/StoreServiceImplTest.java
+++ src/test/java/flab/quing/store/StoreServiceImplTest.java
@@ -1,5 +1,7 @@
 package flab.quing.store;
 
+import flab.quing.store.dto.MenuRequest;
+import flab.quing.store.dto.MenuResponse;
 import flab.quing.store.dto.StoreRequest;
 import flab.quing.store.dto.StoreResponse;
 import flab.quing.user.User;
@@ -34,9 +36,9 @@ class StoreServiceImplTest {
     @Mock
     MenuRepository menuRepository;
 
-    @DisplayName("스토어 저장 성공")
     @Test
-    void addStore() {
+    @DisplayName("스토어 저장 성공")
+    void add_Store_Success() {
         //given
         User user = User.builder()
                 .name("김땡땡")
@@ -77,5 +79,155 @@ class StoreServiceImplTest {
         assertThat(result.getName()).isEqualTo("가게1");
     }
 
+    @Test
+    @DisplayName("메뉴 저장 성공")
+    void add_Menu_Success() {
+        //given
+        User user = User.builder()
+                .name("김땡땡")
+                .phone("010-9999-9999")
+                .build();
+        user.setId(1L);
 
+        Store store = Store.builder()
+                .user(user)
+                .name("가게1")
+                .phoneNumber("02-1234-1234")
+                .openStatus("영업중")
+                .openHours("매일 11:00~23:00")
+                .address("서울특별시 성동구 왕십리로 1")
+                .pageLink("https://github.com/f-lab-edu/quing")
+                .build();
+        store.setId(1L);
+
+        Menu menu1 = Menu.builder()
+                .store(store)
+                .name("시카고피자")
+                .price(22000)
+                .imageUrl("/image/pizza.png")
+                .build();
+        menu1.setId(1L);
+
+        MenuRequest menuRequest1 = MenuRequest.builder()
+                .storeId(store.getId())
+                .name("시카고피자")
+                .price(22000)
+                .imageUrl("/image/pizza.png")
+                .build();
+
+        Menu menu2 = Menu.builder()
+                .store(store)
+                .name("페퍼로니피자")
+                .price(20000)
+                .imageUrl("/image/pepperoniPizza.png")
+                .build();
+        menu2.setId(2L);
+
+        MenuRequest menuRequest2 = MenuRequest.builder()
+                .storeId(store.getId())
+                .name("페퍼로니피자")
+                .price(20000)
+                .imageUrl("/image/pepperoniPizza.png")
+                .build();
+
+        when(storeRepository.findById(1L)).thenReturn(Optional.of(store));
+        when(menuRepository.save(menu1)).thenReturn(menu1);
+        when(menuRepository.save(menu2)).thenReturn(menu2);
+
+        //when
+        MenuResponse result1 = storeService.addMenu(menuRequest1);
+        MenuResponse result2 = storeService.addMenu(menuRequest2);
+
+        //then
+        assertThat(result1.getMenuId()).isEqualTo(1L);
+        assertThat(result1.getName()).isEqualTo("시카고피자");
+        assertThat(result2.getName()).isEqualTo("페퍼로니피자");
+    }
+
+    @Test
+    @DisplayName("스토어 수정 성공")
+    public void update_Store_Success() {
+        //given
+        User user = User.builder()
+                .name("김땡땡")
+                .phone("010-9999-9999")
+                .build();
+        user.setId(1L);
+
+        Store store = Store.builder()
+                .user(user)
+                .name("가게1")
+                .phoneNumber("02-1234-1234")
+                .openStatus("영업중")
+                .openHours("매일 11:00~23:00")
+                .address("서울특별시 성동구 왕십리로 1")
+                .pageLink("https://github.com/f-lab-edu/quing")
+                .build();
+        store.setId(1L);
+
+        StoreRequest storeRequest = StoreRequest.builder()
+                .userId(user.getId())
+                .storeId(store.getId())
+                .name("가게3")
+                .phoneNumber("02-1234-1234")
+                .openStatus("영업중")
+                .openHours("매일 11:00~21:00")
+                .address("서울특별시 성동구 왕십리로 1")
+                .pageLink("https://github.com/f-lab-edu/quing")
+                .build();
+
+        when(storeRepository.findById(1L)).thenReturn(Optional.of(store));
+
+        //when
+        StoreResponse result = storeService.updateStore(storeRequest);
+
+        //then
+        assertThat(result.getName()).isEqualTo("가게3");
+    }
+
+    @Test
+    @DisplayName("메뉴 수정 성공")
+    public void menu_update_success() {
+        //given
+        User user = User.builder()
+                .name("김땡땡")
+                .phone("010-9999-9999")
+                .build();
+        user.setId(1L);
+
+        Store store = Store.builder()
+                .user(user)
+                .name("가게1")
+                .phoneNumber("02-1234-1234")
+                .openStatus("영업중")
+                .openHours("매일 11:00~23:00")
+                .address("서울특별시 성동구 왕십리로 1")
+                .pageLink("https://github.com/f-lab-edu/quing")
+                .build();
+        store.setId(1L);
+
+        Menu menu1 = Menu.builder()
+                .store(store)
+                .name("시카고피자")
+                .price(22000)
+                .imageUrl("/image/pizza.png")
+                .build();
+        menu1.setId(1L);
+
+        MenuRequest menuRequest1 = MenuRequest.builder()
+                .storeId(store.getId())
+                .menuId(menu1.getId())
+                .name("알리오올리오")
+                .price(18000)
+                .imageUrl("/image/pizza.png")
+                .build();
+
+        when(menuRepository.findById(1L)).thenReturn(Optional.of(menu1));
+
+        //when
+        MenuResponse result = storeService.updateMenu(menuRequest1);
+
+        //then
+        assertThat(result.getName()).isEqualTo("알리오올리오");
+    }
 }
