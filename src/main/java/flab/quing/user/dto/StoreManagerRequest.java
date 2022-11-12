11a04bf4cd9e5db097d7da505825612806a7b120
yuseon <llys0331@gmail.com>
2022-11-13 00:37:42 +0900

    #29-생성자 어노테이션 추가

--- src/main/java/flab/quing/user/dto/StoreManagerRequest.java
+++ src/main/java/flab/quing/user/dto/StoreManagerRequest.java
@@ -1,12 +1,16 @@
 package flab.quing.user.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Data
 @Setter(AccessLevel.NONE)
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class StoreManagerRequest {
 
