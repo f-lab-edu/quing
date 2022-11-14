9f834670ab4086228d6f00f9be7de50a29075f1e
Chanwookim <chanwookim@me.com>
2022-11-14 09:26:57 +0900

    #31-ConsoleLogMessageSender 리뷰 반영 - @Component -> @Service - test에서 before each대신 직접 객체생성

--- src/main/java/flab/quing/waiting/ConsoleLogMessageSender.java
+++ src/main/java/flab/quing/waiting/ConsoleLogMessageSender.java
@@ -2,9 +2,11 @@ package flab.quing.waiting;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 @Slf4j
 @Component
+@Service
 public class ConsoleLogMessageSender implements MessageSender {
 
     @Override
--- src/test/java/flab/quing/waiting/ConsoleLogMessageSenderTest.java
+++ src/test/java/flab/quing/waiting/ConsoleLogMessageSenderTest.java
@@ -1,23 +1,15 @@
 package flab.quing.waiting;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @ExtendWith(OutputCaptureExtension.class)
 class ConsoleLogMessageSenderTest {
 
-    MessageSender messageSender;
-
-    @BeforeEach
-    void beforeEach() {
-        messageSender = new ConsoleLogMessageSender();
-    }
+    private final MessageSender messageSender = new ConsoleLogMessageSender();
 
     @Test
     void send(CapturedOutput output) {
