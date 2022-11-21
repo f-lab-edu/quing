a876c5372c2d3167b35dbcecdf5aaf1021397ba1
yuseon <llys0331@gmail.com>
2022-11-21 14:05:15 +0900

    #29-PasswordEncoder.java 파일 미사용으로 삭제

--- src/main/java/flab/quing/util/CustomPasswordEncoder.java
+++ src/main/java/flab/quing/util/CustomPasswordEncoder.java
@@ -1,6 +1,5 @@
 package flab.quing.util;
 
-import flab.quing.util.PasswordEncoder;
 import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigInteger;
--- src/main/java/flab/quing/util/PasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package flab.quing.util;
-
-public interface PasswordEncoder {
-    String hashPassword(String rawPassword);
-
-    Boolean isMatched(String rawPassword, String hashedPassword);
-}
