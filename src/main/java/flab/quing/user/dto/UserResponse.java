730f6dad72df1fe1b65e08fa7217d9f35b7ec052
Chanwookim <chanwookim@me.com>
2022-11-28 07:51:14 +0900

    #64-직렬화 적용

--- src/main/java/flab/quing/user/dto/UserResponse.java
+++ src/main/java/flab/quing/user/dto/UserResponse.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Data
 @Setter(AccessLevel.NONE)
 @Builder
-public class UserResponse {
+public class UserResponse implements Serializable {
 
     private long userId;
 
