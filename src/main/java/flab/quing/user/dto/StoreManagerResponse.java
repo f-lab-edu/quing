842cda853c857c6734d60bd58c6b87cdf4c39848
Chanwookim <chanwookim@me.com>
2022-12-02 08:08:19 +0900

    #64-직렬화 적용 (#65)

--- src/main/java/flab/quing/user/dto/StoreManagerResponse.java
+++ src/main/java/flab/quing/user/dto/StoreManagerResponse.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Data
 @Setter(AccessLevel.NONE)
 @Builder
-public class StoreManagerResponse {
+public class StoreManagerResponse implements Serializable {
 
     private String loginId;
     private String phoneNumber;
